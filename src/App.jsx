e40d0aed5292f0ad5535e7e4f092a9095c1e03a9
Mohamed <61628964+mohamedlaraiche@users.noreply.github.com>
2023-04-27 19:15:55 +0100

    Fetch data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Card from "./components/Card";
 import "./styles/style.css";
+
 const App = () => {
+  const [Datas, setData] = useState([]);
+  const getData = async () => {
+    try {
+      const res = await axios.get(
+        `https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${1}`
+      );
+      const data = await res.data;
+
+      setData(data.items);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <section className="container app">
-      <h1>Github Repos in last 30 days</h1>
+      <h1>GitHub Repos App</h1>
       <div className="cardsHolder">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {Datas.length === 0 ? (
+          <p>Load...</p>
+        ) : (
+          Datas.map((data) => (
+            <Card
+              key={data.id}
+              name={data.name}
+              username={data.owner.login}
+              img={data.owner.avatar_url}
+              desc={data.description}
+              stars={data.stargazers_count}
+              issues={data.open_issues_count}
+              timing={data.size}
+            />
+          ))
+        )}
       </div>
     </section>
   );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,25 @@
+/* eslint-disable react/prop-types */
 import { RiStarSFill } from "react-icons/ri";
 import { GoIssueOpened } from "react-icons/go";
-const Card = () => {
+const Card = ({ name, username, img, desc, stars, issues, timing }) => {
   return (
     <section className="card">
       <aside className="userImgHolder">
-        <img src="./h.png" alt="name" className="userImg" />
+        <img src={img} alt="name" className="userImg" />
       </aside>
       <main className="userDetailsHolder">
-        <h2>Name</h2>
-        <h4>username</h4>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos,
-          quasi!
-        </p>
+        <h2> {name} </h2>
+        <h4>{username}</h4>
+        <p>{desc}</p>
         <section className="moreDetailsHolder">
           <span className="stars">
-            <RiStarSFill /> 20
+            <RiStarSFill /> {stars}
           </span>
           <span className="issues">
             <GoIssueOpened />
-            20
+            {issues}
           </span>
-          <span className="time">timing</span>
+          <span className="time">{timing}</span>
         </section>
       </main>
     </section>
