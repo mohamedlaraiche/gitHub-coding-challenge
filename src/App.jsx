6374c8856f3ec7158892f3d66126e574133f31cc
Mohamed <61628964+mohamedlaraiche@users.noreply.github.com>
2023-04-28 00:00:45 +0100

    updates: fix some responsivness issues

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,14 @@ const App = () => {
         dataLength={Datas.length}
         next={() => setPage(page + 1)}
         hasMore={true}
-        loader={<h4>Loading...</h4>}
+        loader={
+          <h4
+            style={{
+              textAlign: "center",
+            }}>
+            Loading...
+          </h4>
+        }
         endMessage={<p>No more items to load.</p>}>
         {Datas.map((data, index) => (
           <Card
